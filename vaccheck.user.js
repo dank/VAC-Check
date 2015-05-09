d8913ca92aff46ed2e869186d104cdc310165206
Eyepawd Fgt <rmadanny@hotmail.com>
2015-05-08 20:57:07 -0400

    Reset API key by accident /_/

--- vaccheck.user.js
+++ vaccheck.user.js
@@ -6,10 +6,13 @@
 // @author       Eyepawd
 // @contributor  Gat
 // @match        http://steamcommunity.com/*/friends/coplay*
+// @match        https://steamcommunity.com/*/friends/coplay*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @grant        none
 // @updateURL    https://scriptr.org/dl/vaccheck.user.js
 // ==/UserScript==
+
+var api = 'A0A47E806ED927C2F71C9B16EE27FD4D';
 
 var friends = $('.friendCheckbox').toArray();
 var recent = $('.friendCheckbox2').toArray();
@@ -51,13 +54,13 @@ friends.forEach(function(friend) {
 });
 
 function resolveVanityURL(user, cb) {
-    $.get('https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=F6F90A461E30D38AB4AE8AADB5AD8658&vanityurl=' + user, function(data) {
+    $.get('https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + api + '&vanityurl=' + user, function(data) {
         return cb(data.response.steamid);
     });
 }
 
 function getPlayerBans(user, cb) {
-    $.get('https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=F6F90A461E30D38AB4AE8AADB5AD8658&steamids=' + user, function(data) {
+    $.get('https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=' + api + '&steamids=' + user, function(data) {
         return cb(data.players[0]);
     });
 }
